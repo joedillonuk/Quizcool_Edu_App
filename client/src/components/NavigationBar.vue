8b33a32f2153daead7171b70db29dc9165a2aa31
David Eastcroft <davideastcroft95@gmail.com>
2020-06-28 17:04:42 +0100

    Adds icons to nav bar with colour change on selection.

--- client/src/components/NavigationBar.vue
+++ client/src/components/NavigationBar.vue
@@ -1,10 +1,23 @@
 <template lang="html">
   <div class="navigation">
     <nav>
-      <button type="button" v-on:click.once="handleCategory(22)">Geography</button>
+      <!-- <button type="button" v-on:click.once="handleCategory(22)">Geography</button>
       <button type="button" v-on:click.once="handleCategory(21)">Sport</button>
       <button type="button" v-on:click.once="handleCategory(23)">History</button>
-      <button type="button" v-on:click.once="handleCategory(9)">General Knowledge</button>
+      <button type="button" v-on:click.once="handleCategory(9)">General Knowledge</button> -->
+
+      <img v-if="this.selectedCategory !== 22" v-on:click.once="handleCategory(22)" src="../assets/icons/geography-bw.png" alt="geography">
+      <img v-if="this.selectedCategory === 22" v-on:click.once="handleCategory(22)" src="../assets/icons/geography-color.png" alt="geography">
+
+      <img v-if="this.selectedCategory !== 21" v-on:click.once="handleCategory(21)" src="../assets/icons/sport-bw.png" alt="sport">
+      <img v-if="this.selectedCategory === 21" v-on:click.once="handleCategory(21)" src="../assets/icons/sport-color.png" alt="sport">
+
+      <img v-if="this.selectedCategory !== 23" v-on:click.once="handleCategory(23)" src="../assets/icons/history-bw.png" alt="history">
+      <img v-if="this.selectedCategory === 23" v-on:click.once="handleCategory(23)" src="../assets/icons/history-color.png" alt="history">
+
+      <img v-if="this.selectedCategory !== 17" v-on:click.once="handleCategory(17)" src="../assets/icons/science-bw.png" alt="science">
+      <img v-if="this.selectedCategory === 17" v-on:click.once="handleCategory(17)" src="../assets/icons/science-color.png" alt="science">
+
 <user-details/>
     </nav>
 
@@ -46,5 +59,8 @@ export default {
 </script>
 
 <style lang="css" scoped>
-
+img {
+  width: 40px;
+  margin: 10px
+}
 </style>
