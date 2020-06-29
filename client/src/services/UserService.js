caca5e208f17bb219ef5e3471d60ce4dca439169
David Eastcroft <davideastcroft95@gmail.com>
2020-06-29 13:21:58 +0100

    Fixes base URL.

--- client/src/services/UserService.js
+++ client/src/services/UserService.js
@@ -1,23 +1,32 @@
-const baseURL = 'http://localhost:3000/api/users';
+const baseURL = 'http://localhost:3000/api/users/';
 
 export default {
-  getUsers() {
-    return fetch(baseURL)
-    .then(res => res.json());
-  },
+getUsers() {
+  return fetch(baseURL)
+  .then(res => res.json());
+},
 
-  updateUser(user) {
-    return fetch(baseURL, {
-      method: 'POST',
-      body: JSON.stringify(user),
-      headers: { 'Content-Type': 'application/json' },
-    })
-    .then(res => res.json());
-  },
+updateUser(user) {
+  return fetch(baseURL, {
+    method: 'POST',
+    body: JSON.stringify(user),
+    headers: { 'Content-Type': 'application/json' },
+  })
+  .then(res => res.json());
+},
 
-  deleteUser(id) {
-    return fetch(baseURL + id, {
-      method: 'DELETE',
-    });
-  },
+deleteUser(id) {
+  return fetch(baseURL + id, {
+    method: 'DELETE',
+  });
+},
+
+updateExistingUser(id, data){
+  return fetch(baseURL + id, {
+    method: "PUT",
+    body: JSON.stringify(data),
+    headers: { "Content-Type": "application/json" }
+  })
+.then(res => res.json())
+}
 };
