eabdfce4361edfe2acf4ca0780f00b0a823afbf1
David Eastcroft <davideastcroft95@gmail.com>
2020-06-29 13:41:48 +0100

    game end- start

--- client/src/components/UserDetails.vue
+++ client/src/components/UserDetails.vue
@@ -1,13 +1,13 @@
 <template lang="html">
-<div class="">
-  <h2>Hi, {{selectedUser.name}}</h2>
-  <h2>total points: {{selectedUser.points}}</h2>
-  <h2>level: {{selectedUser.level}}</h2>
-  <button type="button" name="button">Log out(?)</button>
-  <h2 class="main-font">You have answered {{currentScore.length}} questions.</h2>
-  <h2 class="main-font" v-if="currentScore.length">Your score is {{totalScore}}.</h2>
-  <h2 class="main-font" v-if="percentage">You have answered {{percentage}}% of questions right.</h2>
-</div>
+  <div v-if="selectedUser">
+    <h2>Hi, {{selectedUser.name}}</h2>
+    <h2>total points: {{selectedUser.points}}</h2>
+    <h2>level: {{selectedUser.level}}</h2>
+    <button type="button" name="button" v-on:click="logOut">Log out</button>
+    <h2 class="main-font">You have answered {{currentScore.length}} questions.</h2>
+    <h2 class="main-font" v-if="currentScore.length">Your score is {{totalScore}}.</h2>
+    <h2 class="main-font" v-if="percentage">You have answered {{percentage}}% of questions right.</h2>
+  </div>
 </template>
 
 <script>
@@ -18,28 +18,22 @@ export default {
   name: 'user-details',
   data(){
     return {
-      users: [],
       currentScore: []
     }
   },
-props: ['selectedUser'],
+  props: ['selectedUser'],
   mounted(){
-      eventBus.$on("send-score", score => {
-        this.currentScore.push(score);
-        this.selectedUser.points += score
-      });
-
+    eventBus.$on("send-score", score => {
+      this.currentScore.push(score);
+      this.sendResult()
+    });
     // eventBus.$on('new-user', (payload) => {
-    //   UserService.updateUser(payload)
-    //   .then(user => {
-    //   this.users.push(user),
+    //     UserService.updateUser(payload)
+    //     .then(user => {this.users.push(user)
+    //       ,
     //   this.selectedUser = user
     // })
-
-
-
-
-      },
+  },
   computed: {
     totalScore: function() {
       return this.currentScore.reduce((sum, current) => sum + current, 0);
@@ -47,20 +41,30 @@ props: ['selectedUser'],
     percentage: function() {
       return Math.round((100 / this.currentScore.length) * this.totalScore);
     },
-    sendScore: function(){
-      if (this.currentScore.length === 3){
-        return this.postUserScore();
+    completedQuiz: function(){
+      if (this.currentScore.length == 3){
+        // this.sleep(3000)
+        return true
+      } else {
+        return false
       }
-    }
+    },
   },
-  methods: {
-    postUserScore(){
-      let points = (this.selectedUser.points += this.totalScore);
-      let id = this.selectedUser._id;
-      const user = {
-        points: points
+  methods:{
+    logOut(){
+      location.reload();
+    },
+    sendResult: function(){
+      if(this.completedQuiz == true){
+        eventBus.$emit('quiz-completed', this.completedQuiz)
       }
-      UserService.updateExistingUser(id, user)
+    },
+    sleep: function(milliseconds) {
+      const date = Date.now();
+      let currentDate = null;
+      do {
+        currentDate = Date.now();
+      } while (currentDate - date < milliseconds);
     }
   }
 }
