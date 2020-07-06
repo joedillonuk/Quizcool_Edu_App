11f4ac3c9839ea77a16bcd3321d332f24ee9b53d
ash-old <ashley.oldham@gmail.com>
2020-07-06 11:01:16 +0100

    added category background and loading spinner

--- client/src/components/QuestionGrid.vue
+++ client/src/components/QuestionGrid.vue
@@ -1,24 +1,29 @@
 <template lang="html">
-<div class="question-font columns">
-  <div class="column is-one-third"></div>
-  <div class="card column is-one-third card-bkg">
-<ol v-for=" question in questions" >
-  <question-item :question="question"/>
-</ol>
-<br>
-<!-- <div class="next" v-if="this.currentScore.length % 5 == 0">
-  <h2><strong>Please select next category</strong></h2>
-</div> -->
-</div>
-<div class="column is-one-third"></div>
-</div>
+
+    <div class="question-font columns">
+      <div class="column is-one-third"></div>
+      <div class="card column is-one-third card-bkg">
+        <spinner v-if="loading" class="spinner"/>
+        <div v-else>
+        <ol v-for=" question in questions" >
+          <question-item :question="question"/>
+        </ol>
+        </div>
+        <br>
+        <!-- <div class="next" v-if="this.currentScore.length % 5 == 0">
+        <h2><strong>Please select next category</strong></h2>
+      </div> -->
+    </div>
+    <div class="column is-one-third"></div>
+  </div>
+
 </template>
 
 <script>
 import QuestionItem from './QuestionItem.vue'
 export default {
   name: 'question-grid',
-  props: ['questions', 'currentScore'],
+  props: ['questions', 'currentScore', 'loading'],
   components:{
     'question-item': QuestionItem
   }
@@ -26,6 +31,11 @@ export default {
 </script>
 
 <style lang="css">
+.spinner{
+  display:block;
+  justify-content: center;
+  margin-left: 210px;
+}
 .question-font{
   font-family: 'Ubuntu', sans-serif;
   font-weight: 500;
